c5c1d1d367f7e60fb7d402f805876487ffb5055d
Neno Neno <nenad.trencevski@gmail.com>
2021-03-14 20:43:34 +0100

    routes moved in a separate file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Routes from './routes';
 import './App.scss';
-import Home from './pages/Home/Home';
 
- const App = () => (
-    <Router>
-      <Switch>
-        <Route path="/" component={Home} />
-      </Switch>
-    </Router>
-  );
+const App = () => (
+  <Routes />
+);
 
 export default App;
